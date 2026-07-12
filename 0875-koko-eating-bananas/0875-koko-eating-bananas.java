ed67f2e63c2e183e4be04bb069376c90099dcfaf
2907-debug <nabiyab86@gmail.com>
2026-07-12 17:01:02 +0530

    Time: 20 ms (34.98%), Space: 47.8 MB (74.54%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,32 +1,42 @@
-class Solution {
+ class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int low=1;
-        int high=findMax(piles);
-        while(low<=high){
-            int mid=(low+high)/2;
-            int totalHours= calculateToatlHours(piles,mid);
-            if(totalHours<=h){
-                high=mid-1;
+        int low = 1;
+        int high = findMax(piles);
 
-            }
-            else{
-                low=mid+1;
+        while (low <= high) {
+             int mid = low + (high - low) / 2;
+
+            long totalHours = calculateTotalHours(piles, mid);
+
+            if (totalHours <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
+
         return low;
     }
-    public int findMax(int[] piles){
-        int maxx=Integer.MIN_VALUE;
-        for(int i=0;i<piles.length;i++){
-            maxx=Math.max(maxx,piles[i]);
+
+    public int findMax(int[] piles) {
+        int maxx = Integer.MIN_VALUE;
+
+        for (int i = 0; i < piles.length; i++) {
+            maxx = Math.max(maxx, piles[i]);
         }
+
         return maxx;
     }
-    public int calculateToatlHours(int[]piles,int hourly){
-        int totalHours=0;
-        for(int i=0;i<piles.length;i++){
-            totalHours +=Math.ceil((double)piles[i]/(double)hourly);
+
+    public long calculateTotalHours(int[] piles, int hourly) {
+        long totalHours = 0;
+
+        for (int i = 0; i < piles.length; i++) {
+            totalHours += (long) Math.ceil(
+                (double) piles[i] / (double) hourly
+            );
         }
+
         return totalHours;
     }
 }
